e14cfe5d9569fa2653e3882a0df20a4809ead690
babylee2002 <babylee2002@gmail.com>
2018-09-11 17:16:51 -0400

    code cleaned

--- src/components/movies.js
+++ src/components/movies.js
@@ -6,30 +6,29 @@ class Movies extends Component {
 
     state = {
         movies: []
-    }
+    };
 
     componentDidMount = () => {
         const movieData = getMovies();
         this.setState({
             movies: movieData
-        })
+        });
     }
     deleteMovie = (id) => {
         const deleted = deleteMovie(id);
         const data = this.state.movies.filter(elem => elem !== deleted)
-        console.log(data)
         this.setState({ movies: data })
     }
 
     render() {
         const tableHead = ['Title', 'Genre', 'Stock', 'Rate', ''];
         const { length: count } = this.state.movies;
-        if (count === 0) return <div className='m-5'> There are no movies available.</div>
+        if (count === 0) return <div className='m-5'> There are no movies available.</div>;
         return <Movie
             movies={this.state.movies}
             tableHead={tableHead}
             deleteMovie={this.deleteMovie}
-            count={count} />
+            count={count} />;
     }
 }
 
