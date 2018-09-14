c6c4216991afe97da6b2abfafae2562b0770aead
babylee2002 <babylee2002@gmail.com>
2018-09-13 21:44:11 -0400

    clean up

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -5,19 +5,23 @@ import _ from 'lodash';
 const Pagination = (props) => {
     const { count, perPage, currentPage, onPageChange, handlePrevious, handleNext } = props;
     const pageCount = Math.ceil(count / perPage);
-    // if (pageCount <= 1) return null;
     const pages = _.range(1, pageCount + 1);
     return <nav>
         <ul className="pagination">
-            <li className="page-item" onClick={() => handlePrevious(currentPage)} value='previous'><a className="page-link" >Previous</a></li>
+            <li
+                className="page-item"
+                onClick={() => handlePrevious(currentPage)}>
+                <a className="page-link" >Previous</a>
+            </li>
             {pages.map(page => <li key={page}
                 className={page === currentPage ? 'page-item active' : 'page-item'}>
                 <a className="page-link"
                     onClick={() => onPageChange(page)}>{page}</a>
             </li>)}
             <li className="page-item"
-                onClick={() => handleNext(currentPage, pageCount)}
-            ><a className="page-link">Next</a></li>
+                onClick={() => handleNext(currentPage, pageCount)}>
+                <a className="page-link">Next</a>
+            </li>
         </ul>
     </nav >
 }
