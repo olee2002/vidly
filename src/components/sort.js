95902e7aa6e98e9f58e43c617c65a30bbb329a36
babylee2002 <babylee2002@gmail.com>
2018-09-14 20:39:10 -0400

    console.log removed

--- src/components/sort.js
+++ src/components/sort.js
@@ -2,17 +2,14 @@ import React from 'react';
 
 const Sort = (props) => {
     const { handleGenre, filteredMovies } = props;
-    console.log('test', filteredMovies)
-
     const genre = ['All Genre', 'Action', 'Comedy', 'Thriller'];
     return <select
         class="custom-select"
         onChange={handleGenre}
         id="inputGroupSelect02">
-        {genre.map(g =>
-            <option
-                value={g}
-                className={"list-group-item"}>{g}</option>
+        {genre.map(g => <option
+            value={g}
+            className={"list-group-item"}>{g}</option>
         )}
     </select>
 }
